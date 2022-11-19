57971a10f831e810214f77659a80439c041080b1
Piotr Szmurlo <pszmurlo@box.com>
2022-11-19 18:11:26 +0100

    start

---
+++ GUI/frontend/src/components/FileUploader.jsx
@@ -0,0 +1,13 @@
+import Button from '@mui/material/Button'
+import Input from '@mui/material/Input'
+import './css/FileUploader.css'
+
+function FileUploader() {
+    return(<div>
+        <Button variant='outlined'>git</Button>
+        <Input type='file'></Input>
+
+    </div>)
+}
+
+export default FileUploader
---
+++ GUI/frontend/src/components/VerificationPage.jsx
@@ -0,0 +1,11 @@
+import FileUploader from "./FileUploader";
+
+function VerificationPage() {
+  return (
+    <div>
+      <FileUploader></FileUploader>
+    </div>
+  );
+}
+
+export default VerificationPage;
