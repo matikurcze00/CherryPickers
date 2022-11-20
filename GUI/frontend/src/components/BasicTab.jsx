22e3c1a94d9c365406c6b51eb756382220123d03
Piotr Szmurlo <pszmurlo@box.com>
2022-11-20 10:32:23 +0100

    basic tabber

---
+++ GUI/frontend/src/components/BasicTab.jsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs(props) {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    props.setTabEnabled(!props.tabEnabled)
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="File Upload" {...a11yProps(0)} />
+          <Tab label="Recents" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+    </Box>
+  );
+}
