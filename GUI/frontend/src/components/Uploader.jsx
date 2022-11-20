a48492467f4ab549257815a934f5e02e800b6b34
Piotr Szmurlo <pszmurlo@box.com>
2022-11-20 01:06:44 +0100

    siema

--- GUI/frontend/src/components/Uploader.jsx
+++ GUI/frontend/src/components/Uploader.jsx
@@ -1,8 +1,16 @@
 import "./css/Uploader.css";
 import React, { useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
-
+// const axios = require('axios');
+import axios, * as others from 'axios';
 const fileTypes = ["PDF"];
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+const onUpload = (file) => {
+  axios.post('http://localhost:3001/file', file)
+    .then(function (response) {
+    console.log(response);
+  })
+}
 
 function Uploader(props) {
 //   let files = [...props.files];
@@ -15,6 +23,7 @@ function Uploader(props) {
         handleChange={(file) => {(props.files == undefined ? 
             props.setFiles(file) :
             props.setFiles(props.files.concat(file)));
+            onUpload(file)
         }}
       ></FileUploader>
     </div>
